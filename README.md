c7d8d94e6672e6ab18186d2a2edf90a86098f73a
theocheng <zlz_gty@foxmail.com>
2023-05-02 19:20:33 +0800

    进行部分修改,以使之可以在Linux上运行

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
+**Fork from [cuifengcn/flet-ui-openai](https://github.com/cuifengcn/flet-ui-openai)**
+
+
 # flet-ui-openai
+对OpenAI API的包装，适用于ChatGPT聊天。
 
-![image](https://user-images.githubusercontent.com/38805177/231632102-8186e726-9c1c-486f-bdf8-bcbeb0593850.png)
+# 更改内容
+- 更换`poetry`进行环境管理，并清理无用软件包
+- 清理获取代理部分的代码，在Linux上无法使用，清理后便可
+- 更改获取API的逻辑，可以直接从环境变量中读取
 
-![image](https://user-images.githubusercontent.com/38805177/231632205-bde696d2-507b-4d26-a4a8-2881f2cf74dc.png)
 
-![image](https://user-images.githubusercontent.com/38805177/231632235-f62be707-0f7f-4574-afda-134560ca0ca4.png)
+
--- main.py
+++ main.py
@@ -1,76 +1,10 @@
-import os
-import re
 import traceback
-import urllib.request
 import uuid
-import winreg
 from typing import Optional, Literal
 
 import flet as ft
 import openai
-from asgiref.sync import sync_to_async
 from loguru import logger
-from requests_html import AsyncHTMLSession
-
-
-async def _get_proxy():
-    try:
-        proxy = winreg.OpenKey(
-            winreg.HKEY_CURRENT_USER,
-            "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings",
-        )
-        server, _type = winreg.QueryValueEx(proxy, "ProxyServer")  # 代理的地址
-        enabled, _type = winreg.QueryValueEx(proxy, "ProxyEnable")  # 是否启用了手动代理
-        auto_config = None
-    except Exception as e:
-        # logger.warning(f"winreg.OpenKey error {e}")
-        return None
-    try:
-        auto_config = winreg.QueryValueEx(proxy, "AutoConfigURL")
-    except FileNotFoundError:
-        pass
-    if enabled == 1:
-        proxy = server
-    elif auto_config is not None:
-        try:
-            config_file = auto_config[0]
-            config_file = await AsyncHTMLSession().get(config_file, timeout=10)
-            content = config_file.text
-            # return "PROXY 127.0.0.1:19180"
-            _reg = re.compile(r'return "PROXY ([0-9a-zA-Z\.:]*)"')
-            _reg2 = re.compile(r"return \'PROXY ([0-9a-zA-Z\.:]*)\'")
-            res = _reg.findall(content)
-            if not res:
-                res = _reg2.findall(content)
-                if not res:
-                    # print("查询代理-正则匹配失败")
-                    return None
-            proxy = res[0]
-        except Exception as e:
-            logger.warning(f"get proxy: {e}")
-            proxy = None
-    else:
-        proxy = None
-    if proxy and not proxy.startswith("http") and proxy[0].isdigit():
-        proxy = "http://" + proxy
-    if proxy and ";" in proxy:
-        proxy = proxy.split(";")[0]
-    if proxy and "=" in proxy:
-        proxy = proxy.replace("=", "://")
-    return proxy
-
-
-async def get_proxy():
-    proxy1 = await sync_to_async(urllib.request.getproxies)()
-    if proxy1 and "http" in proxy1:
-        proxy = proxy1["http"]
-    else:
-        proxy = await _get_proxy()
-    if proxy:
-        os.environ.update(http_proxy=proxy, https_proxy=proxy)
-    else:
-        os.environ.update(http_proxy="", https_proxy="")
-    return proxy
 
 
 async def snack_bar(page, message):
@@ -86,9 +20,10 @@ class Openai:
         self.conversations = []
 
     async def send(self, model, api_key, message):
-        proxy = await get_proxy()
-        self.openai.proxy = proxy
-        self.openai.api_key = api_key
+        # proxy = await get_proxy()
+        # self.openai.proxy = proxy
+        if not self.openai.api_key:
+            self.openai.api_key = api_key
         tmp = self.conversations + [{"role": "user", "content": message}]
         response = await self.openai.ChatCompletion.acreate(
             model=model,
